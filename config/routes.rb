861ca6f95bb3566e9bf6f5a9beb709961431786d
venky <venkykly@gmail.com>
2017-07-09 13:27:58 +0530

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+	
+  devise_for :users
+  devise_for  :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
+   match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
